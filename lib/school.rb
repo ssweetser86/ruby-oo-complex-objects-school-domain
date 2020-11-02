2016513064f5efe17cabf74e93f4db2f5b43e644
Stephen <stephenjsweets@gmail.com>
2020-11-02 16:00:47 -0500

    commit tree 8cc4ff54d5894a90e66e3e1a19332db870dcce6d

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+    attr_reader :roster
+
+    def initialize(school)
+        @school = school
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        if @roster.key?(grade)
+            @roster[grade] << name
+        else
+            @roster[grade] = []
+            @roster[grade] << name
+        end
+    end
+
+    def grade(grade)
+        @roster[grade]    
+    end
+
+    def sort
+        @roster.each do |key, value|
+            @roster[key] = @roster[key].sort
+        end
+    end
+end
